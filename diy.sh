11242f7cc994ea09aa3422d428162a53843b883a
Pastechn <62227537+Pastechn@users.noreply.github.com>
2020-04-01 18:19:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,13 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# update argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+
+# Add luci-app-onliner (need luci-app-nlbwmon)
+git clone https://github.com/rufengsuixing/luci-app-onliner
+
+# fix '?'
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' /usr/lib/lua/luci/view/admin_status/index.htm
