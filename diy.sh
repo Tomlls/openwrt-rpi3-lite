17a646b645c7776d6f7760a6fc8082d456dea5ad
Pastechn <62227537+Pastechn@users.noreply.github.com>
2020-04-01 17:58:32 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' /usr/lib/lua/luci/view/admin_status/index.htm
